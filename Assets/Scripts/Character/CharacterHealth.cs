e9c01ab7702069236d70b0494f1d13d72ad867ad
Andhika Dwi Atamaja Ristiantyo <137518772+AndhikaAtmaja@users.noreply.github.com>
2026-07-18 14:57:05 +0700

    Update Health Bar UI

--- Assets/Scripts/Character/CharacterHealth.cs
+++ Assets/Scripts/Character/CharacterHealth.cs
@@ -7,9 +7,12 @@ public class CharacterHealth : MonoBehaviour
     public float maxHealth;
     public float currentHealth;
 
+    [SerializeField] private StatusBarUI healthBarUI;
+    
     private void Start()
     {
         currentHealth = maxHealth;
+        healthBarUI.UpdateStatusBar(currentHealth, maxHealth);
     }
 
     public void UpdateHealthTakeDamage(float amount)
@@ -21,11 +24,17 @@ public class CharacterHealth : MonoBehaviour
             Debug.Log($"{gameObject.name} is dead");
             owenrCharacter.OnDeadCharacter();
         }
+        
+        healthBarUI.UpdateStatusBar(currentHealth, maxHealth);
+        
     }
 
     public void UpdateHealthRestore(float amount)
     {
         Debug.Log($"{gameObject.name} is restored {amount}");
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        healthBarUI.UpdateStatusBar(currentHealth, maxHealth);
+        
     }
 }
